7b8c32d24b8dfb0d536999961e4e2d0d1e6a53bf
munozgonzalez4 <48728939+munozgonzalez4@users.noreply.github.com>
2026-05-14 10:57:31 +0200

    V1 of the app: returns a Welcome message in the local server

--- src/app.py
+++ src/app.py
@@ -1,3 +1,19 @@
+'''
+This is the main file of the Flask application. It initializes the Flask app and defines the routes for the application.
+'''
+
 from flask import Flask
 
-app = Flask()
+# Create an instance of the Flask class. This instance will be our Web Server Gateway Interface (WSGI) application.
+# __name__ is a parameter that Flask uses to determine the root path of the application. It helps Flask to find resources like templates and static files.
+app = Flask(__name__)
+
+# Create a basic route for the home page
+# The decorator @app.route("/") tells Flask that this function should be called when the root URL ("/") is accessed
+@app.route("/")
+def welcome():
+    return "Welcome to the Flask application!"
+
+
+if __name__ == '__main__':
+    app.run()
