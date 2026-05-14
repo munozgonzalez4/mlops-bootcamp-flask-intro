e45c4008cfe3eb169b073323bf2d59d07f76d18c
munozgonzalez4 <48728939+munozgonzalez4@users.noreply.github.com>
2026-05-14 12:04:58 +0200

    Adding HTML pages via render_template()

--- src/second_class/main.py
+++ src/second_class/main.py
@@ -2,13 +2,25 @@
 Objective: integrate HTML tags
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Manual way to integrate HTML tags: not recommended
 @app.route("/")
 def welcome():
     return "<html><H1>Welcome to the Flask course</H1></html>"
+
+
+# Using templates to integrate HTML tags: recommended way
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
